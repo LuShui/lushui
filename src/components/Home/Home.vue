fe92b7cda357a03b0777f0d084e1d3f55c337f2e
freekoy <1257678024@qq.com>
2018-05-09 20:59:03 +0800

    add

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-<i class="el-icon-delete"></i>
-
+    {{maxValue}}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      maxValue: ''
+    }
+  },
+  created: function () {
+    let a1 = 3
+    let b1 = 6
+    let c1 = 11
+    let d1 = 22
+    let e1 = 1
+    let array = [a1, b1, c1, d1, e1]
+    let max = Math.min.apply(null, array)
+    this.maxValue = max
+  }
 }
 </script>
 
